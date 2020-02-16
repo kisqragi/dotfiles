98f6dc6b1a0186ef3ec89e0b387ec90c91de6685
kisqragi <kisaragi.sec@gmail.com>
2020-02-17 01:00:10 +0900

    gitコマンドがなくてもインストールできるようにした

--- install.sh
+++ install.sh
@@ -6,25 +6,38 @@ has() {
     type "$1" > /dev/null 2>&1
 }
 
-if !(has "git"); then
-    echo "command not found: git"
-else
-    if [ ! -d ${DOT_DIR} ]; then
+if [ ! -d ${DOT_DIR} ]; then
+    if has "git"; then
         git clone https://github.com/kisqragi/dotfiles.git ${DOT_DIR}
-        cd ${DOT_DIR}
-        for f in *
-        do
-            [[ "$f" == ".git" ]] && continue
-            [[ "$f" == ".gitignore" ]] && continue
-            [[ "$f" == ".DS_Store" ]] && continue
-            [[ "$f" == "README.md" ]] && continue
-            [[ "$f" == "install.sh" ]] && continue
-
-            ln -snf $DOT_DIR/"$f" $HOME/".$f"
-            echo "Installed .$f"
-        done
+    elif has "curl" || has "wget"; then
+        TARBALL="https://github.com/kisqragi/dotfiles/archive/master.tar.gz"
+        TARBALL_NAME="dotfiles.tar.gz"
+        if has "curl"; then
+            curl -L ${TARBALL} -o ${TARBALL_NAME}
+        else
+            wget ${TARBALL} -O ${TARBALL_NAME}        
+        fi
+        tar -zxvf ${TARBALL_NAME}
+        mv -f dotfiles-master "${DOT_DIR}"
     else
-        cd ${DOT_DIR}
-        git pull
+        echo "curl or wget or git required"
+        exit 1
     fi
+
+    cd ${DOT_DIR}
+    for f in *
+    do
+        [[ "$f" == ".git" ]] && continue
+        [[ "$f" == ".gitignore" ]] && continue
+        [[ "$f" == ".DS_Store" ]] && continue
+        [[ "$f" == "README.md" ]] && continue
+        [[ "$f" == "install.sh" ]] && continue
+
+        ln -snf $DOT_DIR/"$f" $HOME/".$f"
+        echo "Installed .$f"
+    done
+else
+    echo "dotfiles already exists"
+    exit 1
 fi
+
