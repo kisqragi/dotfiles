ac0e068dba34be137833cc4a87313dbabc646a46
kisqragi <kisaragi.sec@gmail.com>
2020-02-16 20:10:55 +0900

    ワンライナーでできるようにした

--- install.sh
+++ install.sh
@@ -2,13 +2,29 @@
 
 DOT_DIR="$HOME/dotfiles"
 
-for f in ??*
-do
-    [[ "$f" == ".git" ]] && continue
-    [[ "$f" == ".gitignore" ]] && continue
-    [[ "$f" == ".DS_Store" ]] && continue
-    [[ "$f" == "README.md" ]] && continue
+has() {
+    type "$1" > /dev/null 2>&1
+}
 
-    ln -sf $DOT_DIR/"$f" $HOME/".$f"
-    echo "Installed .$f"
-done
+if !(has "git"); then
+    echo "command not found: git"
+else
+    if [ ! -d ${DOT_DIR} ]; then
+        git clone https://github.com/kisqragi/dotfiles.git ${DOT_DIR}
+        cd ${DOT_DIR}
+        for f in *
+        do
+            [[ "$f" == ".git" ]] && continue
+            [[ "$f" == ".gitignore" ]] && continue
+            [[ "$f" == ".DS_Store" ]] && continue
+            [[ "$f" == "README.md" ]] && continue
+            [[ "$f" == "install.sh" ]] && continue
+
+            ln -snf $DOT_DIR/"$f" $HOME/".$f"
+            echo "Installed .$f"
+        done
+    else
+        cd ${DOT_DIR}
+        git pull
+    fi
+fi
