18858769c8fb4462738fdb333f98c9f39068574b
kisqragi <45789218+kisqragi@users.noreply.github.com>
2020-12-20 13:41:25 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ if [ ! -d ${DOT_DIR} ]; then
     if [ $(uname) == "Darwin" ]; then
         ln -s "${DOT_DIR}/yabai/skhdrc/ $HOME/.skhdrc"
         ln -s "${DOT_DIR}/yabai/yabairc/ $HOME/.yabairc"
-        mkdir "$HOME/.config/yabai"
+        mkdir -p "$HOME/.config/yabai"
         cp "${DOT_DIR}/yabai/open_iterm2.sh" "$HOME/.config/yabai/open_iterm2.sh"
         chmod +x "$HOME/.config/yabai/open_iterm2.sh"
     fi
